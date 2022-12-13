c52ce440ae6034811b2c17c1d604969a8d630b9a
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2022-12-13 14:52:33 +0300

    add block Recently Viewed on product page

--- [18465] РГК внесение правок на тестовый/[#27] Вы также смотрели/element_normal.php
+++ [18465] РГК внесение правок на тестовый/[#27] Вы также смотрели/element_normal.php
@@ -124,6 +124,145 @@ $bShowLeftBlock &= !defined('ERROR_404');
 				);?>
 			<?endif;?>
 		</div>
+		
+		<?// Блок "Вы также смотрели" ?>
+		<?$APPLICATION->IncludeComponent(
+			"bitrix:catalog.products.viewed",
+			".default",
+			Array(
+				"ACTION_VARIABLE" => "action_cpv",
+				"ADD_PROPERTIES_TO_BASKET" => "Y",
+				"ADD_TO_BASKET_ACTION" => "ADD",
+				"BASKET_URL" => "/personal/basket.php",
+				"CACHE_GROUPS" => "Y",
+				"CACHE_TIME" => "3600",
+				"CACHE_TYPE" => "A",
+				"COMPONENT_TEMPLATE" => ".default",
+				"CONVERT_CURRENCY" => "N",
+				"DEPTH" => "",
+				"DISPLAY_COMPARE" => "N",
+				"ENLARGE_PRODUCT" => "STRICT",
+				"HIDE_NOT_AVAILABLE" => "N",
+				"HIDE_NOT_AVAILABLE_OFFERS" => "N",
+				"IBLOCK_ID" => "54",
+				"IBLOCK_MODE" => "single",
+				"IBLOCK_TYPE" => "aspro_allcorp3_catalog",
+				"LABEL_PROP_POSITION" => "top-left",
+				"MESS_BTN_ADD_TO_BASKET" => "В корзину",
+				"MESS_BTN_BUY" => "Купить",
+				"MESS_BTN_DETAIL" => "Подробнее",
+				"MESS_BTN_SUBSCRIBE" => "Подписаться",
+				"MESS_NOT_AVAILABLE" => "Нет в наличии",
+				"PAGE_ELEMENT_COUNT" => "6",
+				"PARTIAL_PRODUCT_PROPERTIES" => "N",
+				"PRICE_CODE" => array(),
+				"PRICE_VAT_INCLUDE" => "Y",
+				"PRODUCT_BLOCKS_ORDER" => "price,props,sku,quantityLimit,quantity,buttons",
+				"PRODUCT_ID_VARIABLE" => "id",
+				"PRODUCT_PROPS_VARIABLE" => "prop",
+				"PRODUCT_QUANTITY_VARIABLE" => "quantity",
+				"PRODUCT_ROW_VARIANTS" => "[{'VARIANT':'6','BIG_DATA':false}]",
+				"PRODUCT_SUBSCRIPTION" => "N",
+				"SECTION_CODE" => "",
+				"SECTION_ELEMENT_CODE" => "",
+				"SECTION_ELEMENT_ID" => $GLOBALS["CATALOG_CURRENT_ELEMENT_ID"],
+				"SECTION_ID" => $GLOBALS["CATALOG_CURRENT_SECTION_ID"],
+				"SHOW_CLOSE_POPUP" => "N",
+				"SHOW_DISCOUNT_PERCENT" => "N",
+				"SHOW_FROM_SECTION" => "N",
+				"SHOW_MAX_QUANTITY" => "N",
+				"SHOW_OLD_PRICE" => "N",
+				"SHOW_PRICE_COUNT" => "1",
+				"SHOW_SLIDER" => "Y",
+				"SLIDER_INTERVAL" => "3000",
+				"SLIDER_PROGRESS" => "Y",
+				"TEMPLATE_THEME" => "green",
+				"USE_ENHANCED_ECOMMERCE" => "N",
+				"USE_PRICE_COUNT" => "N",
+				"USE_PRODUCT_QUANTITY" => "N"
+			)
+		);?>
+		<?// end Блок "Вы также смотрели"?>
+		<?// Вывод блока "Вы также смотрели" по-другому?>
+		<?
+		$arViewed = [];
+		$basketUserId = (int) CSaleBasket::GetBasketUserID(false);
+		if ($basketUserId > 0) {
+		$viewedIterator = \Bitrix\Catalog\CatalogViewedProductTable::getList([
+		'select' => ['PRODUCT_ID', 'ELEMENT_ID'],
+		'filter' => ['=FUSER_ID' => $basketUserId, '=SITE_ID' => SITE_ID],
+		'order' => ['DATE_VISIT' => 'DESC'],
+		'limit' => 10
+		]);
+		while ($arFields = $viewedIterator->fetch()) {
+		$arViewed[] = $arFields['ELEMENT_ID'];
+		}
+		}
+		$cnt = count($arViewed);
+
+		/*echo '<pre>';
+		print_r($arViewed);
+		echo '</pre>';*/
+		$arParams["T_GOODS"] = 'Вы также смотрели';
+		?>
+		<?if(!isset($html_goods)):?>
+			<?$GLOBALS['arrGoodsFilter'] = array('ID' => $arViewed);?>
+			<?
+			$bCheckAjaxBlock = CAllcorp3::checkRequestBlock("goods-list-inner");
+			$isAjax = (CAllcorp3::checkAjaxRequest() && $bCheckAjaxBlock ) ? 'Y' : 'N';
+			?>
+			<?ob_start();?>
+					<?\Aspro\Functions\CAsproAllcorp3::showBlockHtml([
+							'FILE' => '/detail_linked_goods.php',
+							'PARAMS' => array_merge(
+									$arParams,
+									array(
+											'ORDER_VIEW' => $bOrderViewBasket,
+											'ITEM_1200' => '6',
+											'CHECK_REQUEST_BLOCK' => $bCheckAjaxBlock,
+						'IS_AJAX' => $isAjax,
+									)
+							)
+					]);?>
+			<?$html_goods = trim(ob_get_clean());?>
+	<?endif;?>
+
+				<?if($html_goods && strpos($html_goods, 'error') === false):?>
+						<?if($bTab):?>
+								<?if(!isset($bShow_goods)):?>
+										<?$bShow_goods = true;?>
+								<?else:?>
+										<div class="tab-pane <?=(!($iTab++) ? 'active' : '')?>" id="goods">
+												<div class="ordered-block__title switcher-title font_22"><?=$arParams["T_GOODS"]?></div>
+												<div class="ajax-pagination-wrapper" data-class="goods-list-inner">
+														<?if ($isAjax === 'Y'):?>
+																<?$APPLICATION->RestartBuffer();?>
+														<?endif;?>
+																<?=$html_goods?>
+														<?if ($isAjax === 'Y'):?>
+																<?die();?>
+														<?endif;?>
+												</div>
+										</div>
+								<?endif;?>
+						<?else:?>
+								<div class="detail-block ordered-block goods">
+										<div class="ordered-block__title switcher-title font_22"><?=$arParams["T_GOODS"]?></div>
+										<div class="ajax-pagination-wrapper" data-class="goods-list-inner">
+												<?if ($isAjax === 'Y'):?>
+														<?$APPLICATION->RestartBuffer();?>
+												<?endif;?>
+														<?=$html_goods?>
+												<?if ($isAjax === 'Y'):?>
+														<?die();?>
+												<?endif;?>
+										</div>
+								</div>
+						<?endif;?>
+				<?endif;?>
+
+					<?// end?>
+		<?// end Вывод блока "Вы также смотрели" по-другому?>
 	</div>
 	<?if($bShowLeftBlock):?>
 		<?CAllcorp3::ShowPageType('left_block');?>
